f6ab20ff5976e1a88c421c40ec99d75389a32117
martinbagge <martinbagge@pm.me>
2023-07-22 21:50:33 +0200

    trying to make it work on serverless

--- hypocreales-api/src/index.ts
+++ hypocreales-api/src/index.ts
@@ -3,6 +3,9 @@ import "dotenv/config";
 import express from "express";
 import { connect } from "mongoose";
 import router from "./routes/internal_api";
+
+import serverless from "serverless-http";
+
 const app = express();
 const port = process?.env?.PORT ?? 5000;
 
@@ -12,9 +15,13 @@ app.get("/", (req, res) => {
   res.send("What are you doing here?");
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+if (process?.env?.ENVIRONMENT == "production") {
+  exports.handler = serverless(app);
+} else {
+  app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+  });
+}
 
 (async () => {
   await connect(process?.env?.MONGO ?? "");
