5c1b343a73645a34c146fea64c076fc24d7ed636
martinbagge <martinbagge@pm.me>
2023-07-23 22:55:21 +0200

    seems to have base path running but all other endpoints are not working

--- hypocreales-api/src/index.ts
+++ hypocreales-api/src/index.ts
@@ -1,32 +1,16 @@
 import "dotenv/config";
 
 import express from "express";
-import { connect } from "mongoose";
 import router from "./routes/internal_api";
 
 import serverless from "serverless-http";
 
 const app = express();
-const port = process?.env?.PORT ?? 5000;
 
 app.use(router);
 
 app.get("/", (req, res) => {
-  res.send("What are you doing here?");
+  res.status(200).send("What are you doing here?");
 });
-
-//if (process?.env?.ENVIRONMENT == "production") {
-
-//} else {
-//  app.listen(port, () => {
-//    return console.log(`Express is listening at http://localhost:${port}`);
-//  });
-//}
-
-(async () => {
-  await connect(process?.env?.MONGO ?? "").then(() =>
-    console.log("DB connected")
-  );
-})();
 
 export const handler = serverless(app);
--- hypocreales-api/src/routes/internal_api.ts
+++ hypocreales-api/src/routes/internal_api.ts
@@ -4,40 +4,55 @@ import { DataPull, PriceDay, Price } from "../types/prices";
 import { MongoPrice } from "../models/prices";
 import { ControllerInfo } from "../types/edge-controllers";
 import { MongoController } from "../models/edge-controllers";
+import * as mongoose from "mongoose";
+
+async function mongo_connect() {
+  if (mongoose.connection.readyState == 1) {
+    return;
+  }
+  await mongoose.connect(process?.env?.MONGO ?? "");
+  return;
+}
+
+
 const router = Router();
 
 router.get("/pull", async (req, res) => {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  const price = await axios.get(
-    `https://www.elprisenligenu.dk/api/v1/prices/${tomorrow.getFullYear()}/${
-      tomorrow.getMonth() < 10 ? "0" : ""
-    }${tomorrow.getMonth()}-${
-      tomorrow.getDay() < 10 ? "0" : ""
-    }${tomorrow.getDay()}_DK1.json`
-  );
-  console.log("just pulled data");
-  if (price.status == 200) {
-    const data = price.data as [DataPull];
-    const prices: Price[] = [];
-    data.forEach((d) => {
-      prices.push({
-        dkk_kwh: d.DKK_per_kWh,
-        time_start: d.time_start,
-        time_end: d.time_end,
+  try {
+    mongo_connect();
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const price = await axios.get(
+      `https://www.elprisenligenu.dk/api/v1/prices/${tomorrow.getFullYear()}/${tomorrow.getMonth() < 10 ? "0" : ""
+      }${tomorrow.getMonth()}-${tomorrow.getDay() < 10 ? "0" : ""
+      }${tomorrow.getDay()}_DK1.json`
+    );
+    console.log("just pulled data");
+    if (price.status == 200) {
+      const data = price.data as [DataPull];
+      const prices: Price[] = [];
+      data.forEach((d) => {
+        prices.push({
+          dkk_kwh: d.DKK_per_kWh,
+          time_start: d.time_start,
+          time_end: d.time_end,
+        });
       });
-    });
-    const mongoprice = new MongoPrice({
-      _id: { date: new Date().setHours(0, 0, 0, 0) },
-      hour_prices: prices,
-    });
-    await mongoprice.save();
+      const mongoprice = new MongoPrice({
+        _id: { date: new Date().setHours(0, 0, 0, 0) },
+        hour_prices: prices,
+      });
+      await mongoprice.save();
+    }
+    res.status(200).send("pulled data");
+  } catch (e: any){
+    res.status(500).send("something went wrong   " + e.message);
   }
-  res.status(200).send("pulled data");
 });
 
 router.get("/start/:mac", async (req, res) => {
+  mongo_connect();
   const mac = req.params.mac;
   const controller: ControllerInfo | null = await MongoController.findOne({
     "_id.mac": mac,
